e44c7cf2b46a6c675ff464739e7c3cf5d9f5c57b
piglovesyou <thepiglovesyou@gmail.com>
2014-02-08 15:20:42 +0900

    rafac and enable fetching soy_msg

--- run.sh
+++ run.sh
@@ -6,8 +6,17 @@
 
 USAGE_TEXT="\n
     ---- Usage ---- \n\n\
+    setup
+    setup_plovr
+    setup_closurelibrary
+    setup_closurestylesheets
+    setup_closuretemplates
+    cleanup_lib
+    soyweb
+    serve
+    build
+    extract_msg
         \n"
-
 
 LIBS_DIR=libs/
 
@@ -19,49 +28,87 @@ PLOVR_JAR_PATH=${PLOVR_DIR}${PLOVR_JAR}
 CLOSURELIBRARY_DIR=${LIBS_DIR}closure-library/
 CLOSURELIBRARY_REMOTE_DIR=http://closure-library.googlecode.com/svn/trunk/
 
-CLOSURETEMPLATE_DIR=${LIBS_DIR}closure-template/
-CLOSURETEMPLATE_REMOTE_DIR=http://closure-templates.googlecode.com/svn/trunk/
-
 CLOSURESTYLESHEETS_JAR=closure-stylesheets-20111230.jar
 CLOSURESTYLESHEETS_DIR=${LIBS_DIR}closure-stylesheets/
 CLOSURESTYLESHEETS_REMOTE_DIR=https://closure-stylesheets.googlecode.com/files/
 CLOSURESTYLESHEETS_JAR_PATH=${CLOSURESTYLESHEETS_DIR}${CLOSURESTYLESHEETS_JAR}
 
+CLOSURETEMPLATES_DIR=${LIBS_DIR}closure-template/
+CLOSURETEMPLATES_REMOTE_DIR=http://closure-templates.googlecode.com/files/
+CLOSURETEMPLATES_FOR_JAVA=closure-templates-for-java-latest.zip
+CLOSURETEMPLATES_FOR_JS=closure-templates-for-javascript-latest.zip
+CLOSURETEMPLATES_MSG_EXTRACTOR=closure-templates-msg-extractor-latest.zip
+
+
+
+cleanup_lib() {
+    mkdir ${LIBS_DIR} > /dev/null 2>&1
+}
+
+setup_plovr() {
+    rm -rf ${PLOVR_DIR}
+    wget -P ${PLOVR_DIR} ${PLOVR_REMOTE_DIR}${PLOVR_JAR}
+}
+
+setup_closurelibrary() {
+    rm -rf ${CLOSURELIBRARY_DIR}
+    (cd ${LIBS_DIR} && svn co ${CLOSURELIBRARY_REMOTE_DIR} closure-library)
+}
+
+setup_closurestylesheets() {
+    rm -rf ${CLOSURESTYLESHEETS_DIR}
+    wget -P ${CLOSURESTYLESHEETS_DIR} --no-check-certificate ${CLOSURESTYLESHEETS_REMOTE_DIR}${CLOSURESTYLESHEETS_JAR}
+}
+
+setup_closuretemplates() {
+    rm -rf ${CLOSURETEMPLATES_DIR}
+    wget -P ${CLOSURETEMPLATES_DIR} ${CLOSURETEMPLATES_REMOTE_DIR}${CLOSURETEMPLATES_FOR_JAVA}
+    unzip -d ${CLOSURETEMPLATES_DIR}java ${CLOSURETEMPLATES_DIR}${CLOSURETEMPLATES_FOR_JAVA}
+    wget -P ${CLOSURETEMPLATES_DIR} ${CLOSURETEMPLATES_REMOTE_DIR}${CLOSURETEMPLATES_FOR_JS}
+    unzip -d ${CLOSURETEMPLATES_DIR}js ${CLOSURETEMPLATES_DIR}${CLOSURETEMPLATES_FOR_JS}
+    wget -P ${CLOSURETEMPLATES_DIR} ${CLOSURETEMPLATES_REMOTE_DIR}${CLOSURETEMPLATES_MSG_EXTRACTOR}
+    unzip -d ${CLOSURETEMPLATES_DIR}msg ${CLOSURETEMPLATES_DIR}${CLOSURETEMPLATES_MSG_EXTRACTOR}
+}
+
+extract_msg() {
+    java -jar \
+        ${SOY_MSG_EXTRACTOR_DIR}${SOY_MSG_EXTRACTOR_JAR} \
+        --outputFile extracted.xlf \
+        $(find "public/app/soy/" -name "*.soy")
+    [ $? -eq 0 ] && echo "extracted.xlf created"
+}
+
+
 
 case $1 in
+
     setup)
-        mkdir ${LIBS_DIR} > /dev/null 2>&1
-        PWD=`pwd`
-
-        # Download plovr
-        rm -rf ${PLOVR_DIR}
-        wget -P ${PLOVR_DIR} ${PLOVR_REMOTE_DIR}${PLOVR_JAR}
-
-        # Download Closure Library
-        rm -rf ${CLOSURELIBRARY_DIR}
-        cd ${LIBS_DIR}
-        svn co -r 2519 ${CLOSURELIBRARY_REMOTE_DIR} closure-library
-        cd ${PWD}
-
-        # Download Closure Stylesheets
-        rm -rf ${CLOSURESTYLESHEETS_DIR}
-        wget -P ${CLOSURESTYLESHEETS_DIR} --no-check-certificate ${CLOSURESTYLESHEETS_REMOTE_DIR}${CLOSURESTYLESHEETS_JAR}
-
+        cleanup_lib
+        setup_plovr
+        setup_closurelibrary
+        setup_closurestylesheets
+        setup_closuretemplates
         ;;
 
-    soyweb)
-        java -jar ${PLOVR_JAR_PATH} soyweb --dir ./public
-        ;;
+    cleanup_lib) cleanup_lib;;
 
-    serve)
-        java -jar ${PLOVR_JAR_PATH} serve plovr.json
-        ;;
+    setup_plovr) setup_plovr;;
 
-    build)
-        java -jar ${PLOVR_JAR_PATH} build plovr.json
-        ;;
+    setup_closurelibrary) setup_closurelibrary;;
 
-    *)
-        echo -e $USAGE_TEXT
-        ;;
+    setup_closurestylesheets) setup_closurestylesheets;;
+
+    setup_closuretemplates) setup_closuretemplates;;
+
+    soyweb) java -jar ${PLOVR_JAR_PATH} soyweb --dir ./public;;
+
+    serve) java -jar ${PLOVR_JAR_PATH} serve plovr.json;;
+
+    build) java -jar ${PLOVR_JAR_PATH} build plovr.json;;
+
+    extract_msg) extract_msg;;
+
+    *) echo -e $USAGE_TEXT;;
+
 esac
+
